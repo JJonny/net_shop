9f20c5a30073979e0631cfdcf8096c6d83e68e26
JJonny <qweer.baranovsky@gmail.com>
2018-01-28 11:15:52 +0300

    Merge branch 'HEAD' into net_shop/master

--- shop/urls.py
+++ shop/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^(?P<category_id>\d+)/$', views.tea_category, name='tea'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 
@@ -6,4 +6,11 @@ def home(request):
     return render(request, "home.html",
                     {'category': Category.objects.all(),
                      'products': Product.objects.all()})
+
+
+def tea_category(request, category_id):
+    tea_list = Product.objects.filter(prod_category=category_id)
+    return render(request, "tea.html", {'category': Category.objects.all(),
+                                        'tea_list': tea_list})
+
 
